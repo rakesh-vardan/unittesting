7703a5e9c26b6a0b84433886c8655b1e3629684f
rakeshb <rakeshlearnit@gmail.com>
2017-05-04 20:07:16 +0530

    Add parameterized method using Junit.

--- src/main/java/org/learn/junit/unittesting/Calculation.java
+++ src/main/java/org/learn/junit/unittesting/Calculation.java
@@ -1,9 +1,14 @@
 package org.learn.junit.unittesting;
 
-public class Addition {
+public class Calculation {
 
 	public int calcSum(int num1, int num2)
 	{
 		return num1+num2;
+	}
+	
+	public int multiply(int num1, int num2)
+	{
+		return num1*num2;
 	}
 }
--- src/test/java/org/learn/junit/unittesting/CalculationTest.java
+++ src/test/java/org/learn/junit/unittesting/CalculationTest.java
@@ -2,17 +2,36 @@ package org.learn.junit.unittesting;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
-public class AdditionTest {
+public class CalculationTest {
 	
-	static Addition add;
+	static Calculation add;
+	
+	@Parameter(0)
+	public int value1;
+	@Parameter(1)
+	public int value2;
+	@Parameter(2)
+	public int result;
+	
+	@Parameters
+	public Collection<Object[]> data()
+	{
+		Object[][] data = new Object[][]{{1,2,2}, {2,3,6}, {5,3,15}};
+		return Arrays.asList(data);
+	}
 	
 	@BeforeClass
 	public static void setUp()
 	{
-		add = new Addition();
+		add = new Calculation();
 	}
 	
 	@Test
@@ -27,5 +46,32 @@ public class AdditionTest {
 	{
 		int result = add.calcSum(-2, -5);
 		assertEquals(-7, result);
+	}
+	
+	@Test
+	public void multiplyPositiveNumbers()
+	{
+		int result = add.multiply(2, 3);
+		assertEquals(6, result);
+	}
+	
+	@Test
+	public void multiplyNegativeNumbers()
+	{
+		int result = add.multiply(-2, -3);
+		assertEquals(6, result);
+	}
+	
+	@Test
+	public void multiplyDifferentNumbers()
+	{
+		int result = add.multiply(-2, 3);
+		assertEquals(-6, result);
+	}
+
+	@Test
+	public void testMultiplyParam()
+	{
+		assertEquals("Result:", result, add.multiply(value1, value2));
 	}
 }
